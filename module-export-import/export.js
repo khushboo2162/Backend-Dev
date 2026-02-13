817954a559f7dfad14e1ef0ecd512c66073ad26c
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-02-13 14:30:27 +0530

    Add file

---
+++ module-export-import/export.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+    console.log(a + b);
+}
+
+function sub(a, b) {
+    console.log(a - b);
+}
+
+function multiply(a, b) {
+    console.log(a * b);
+}
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+module.exports = { add, sub, multiply, divide };
---
+++ module-export-import/import.js
@@ -0,0 +1,5 @@
+const { add, sub, multiply, divide } = require('./export');
+add(5,7);
+sub(90,62);
+multiply(5,5);
+divide(8,2);
