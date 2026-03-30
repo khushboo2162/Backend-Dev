aa0cd5a71ac633fd62e9ef4253d60a099aafab06
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-03-30 14:54:13 +0530

    Removed Cookie folder

--- cookies/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-const app = express();
-app.use(cookieParser("my -super -secret-key"));
-app.get("/", (req, res) => {
-    res.cookie("name", "rohan ,{httpOnly:true}");
-    res.send("cookie set");
-});
-app.listen(3000, () =>  console.log("server is running on port 3000"));
